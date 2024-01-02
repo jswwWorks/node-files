9e30b7390d889ae0f3f06e3107a320723f354cfc
jswwWorks <jsww.works@gmail.com>
2024-01-02 15:32:41 -0500

    Progress on Step 1

---
+++ step1.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const fsP = require("fs/promises");
+
+const argv = process.argv;
+
+const filePath = argv[argv.length - 1];
+console.log('file path we care about', filePath);
+cat(filePath);
+
+// for (let i = 0; i < argv.length; i++) {
+//   console.log(i, argv);
+// }
+
+/** Takes a string, path, and reads file with that string.
+ *  Prints that file.
+ *
+ *  TODO: add more details as we go
+ *
+ * @param {file path} path
+ */
+
+async function cat(path) {
+
+  try {
+    const file = await fsP.readfile(path, "utf8");
+    console.log("this file's contents:", file);
+  } catch {
+    console.log("problem with operation");
+    process.exit(1);
+  }
+
+}
+
+
+
+// await cat('./testCat.txt');
